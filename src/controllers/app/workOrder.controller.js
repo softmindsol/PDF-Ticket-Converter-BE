5f0f4025b1492be85ac8a85f8a46565f7a719160
softmindsol <softmindsol@gmail.com>
2025-10-15 17:13:24 +0500

    Update workOrder.controller.js

--- src/controllers/app/workOrder.controller.js
+++ src/controllers/app/workOrder.controller.js
@@ -4,7 +4,17 @@ import WorkOrder from "#models/workOrder.model.js";
 
 const WorkOrderTicket = asyncHandler(async (req, res) => {
   const body = req.body;
+  const { jobNumber } = body;
   const { _id } = req.user;
+
+  const existingWorkOrder = await WorkOrder.findOne({ jobNumber });
+
+  if (existingWorkOrder) {
+    throw new ApiError(
+      httpStatus.CONFLICT,
+      "A work order with this job number already exists."
+    );
+  }
 
   const ticket = await WorkOrder.create({ ...body, createdBy: _id });
 
